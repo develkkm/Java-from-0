79d24acb445f61e1bfa25d5f205cb96f667c0141
develkkm <kkm8905508@daum.net>
2025-09-02 15:20:45 +0900

    feat: Hello Java 출력하기

---
+++ src/javaStart/HelloJava2.java
@@ -0,0 +1,12 @@
+package javaStart;
+
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // print Hello Java 1
+        //System.out.println("Hello Java2");
+
+        /*
+        System.out.println("Hello Java3");
+        */
+    }
+}
