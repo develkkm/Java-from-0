1e2ba842dbd89667c64d63175d9be8a64c8754bb
develkkm <kkm8905508@daum.net>
2025-09-02 19:02:49 +0900

    feat: Java 변수 - 변수 선언 및 초기화 - 변수 종류 및 리터럴 범위

---
+++ src/javaStart/variable/Var7.java
@@ -0,0 +1,18 @@
+package javaStart.variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //boolean true or false
+        char d = 'a'; //문자
+        String e = "Hello Java"; //문자열을 다루기위한 특별한 타입
+        //직접 작성한 고정된 값 = 리터럴
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
